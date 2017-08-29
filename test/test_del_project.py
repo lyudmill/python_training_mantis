89664c7a5b2b60b7f033e255f4841478bd8486cf
Ludmila Lee <ludlee@yandex.ru>
2017-08-29 19:00:42 +0300

    N26 Add SOAP verificatio to delete project test

--- test/test_del_project.py
+++ test/test_del_project.py
@@ -2,6 +2,19 @@ from model.project import Project
 import random
 
 
+
+def test_delete_project_soap(app):
+    if app.project.count() == 0:
+        app.project.create(Project(name="name for delete"))
+    old_projects = app.soap.get_project_list(app.config['webadmin']["username"], app.config['webadmin']["password"])
+    project = random.choice(old_projects)
+    print(project)
+    app.project.delete_project_by_name(project.name)
+    new_projects = app.soap.get_project_list(app.config['webadmin']["username"], app.config['webadmin']["password"])
+    assert len(old_projects) - 1 == len(new_projects)
+    old_projects.remove(project)
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
+
 def test_delete_project(app):
     if app.project.count() == 0:
         app.project.create(Project(name="name for delete"))
